d84be86f590874179dbf2ebe6ea1f00bee59a404
Pablo Silva <psspqssq@gmail.com>
2021-11-27 22:17:38 -0800

    the duck is mad at me

--- water-test-ui/water-tests/src/Components/Dashboard/DashboardContent/index.js
+++ water-test-ui/water-tests/src/Components/Dashboard/DashboardContent/index.js
@@ -1,40 +1,18 @@
 import React, { useContext, useEffect, useState } from "react"
 import { DashboardContext } from ".."
 import "antd/dist/antd.css"
-import { Layout, Spin } from "antd"
+import { Layout } from "antd"
 import AccountContent from "../AccountContent"
 import SeekWaterRecord from "../WaterTreatmentSubmenu/SeekWaterRecord"
 import RegisterWaterRecord from "../WaterTreatmentSubmenu/RegisterWaterRecord"
 import SearchShop from "../ShopInventorySubmenu/SearchShop"
 import RegisterShop from "../ShopInventorySubmenu/RegisterShop"
-import axios from "axios"
-import useToken from "../../../Middleware/useToken"
 
 const { Content } = Layout
 export const DashboardContent = () => {
   const { selectedTab, selectedMenu } = useContext(DashboardContext)
-  const [data, setData] = useState(null)
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
-  const { token, setToken } = useToken()
-
-  useEffect(() => {
-    axios("http://localhost:8080/api/boilerwater", { params: { token } })
-      .then((response) => {
-        setData(response.data)
-        console.log(response)
-      })
-      .catch((error) => {
-        setError(error)
-      })
-      .finally(() => {
-        setLoading(false)
-      })
-  }, [token])
-
-  if (loading) {
-    return <Spin delay="50" style={{ margin: "auto" }} />
-  }
+  
+ 
   return (
     <Content
       className="site-layout-background"
@@ -45,7 +23,7 @@ export const DashboardContent = () => {
         height: "100vh",
       }}
     >
-      {selectedTab === 1 && selectedMenu === "SeekBoiler" && <SeekWaterRecord type="Boiler" data={data} error={error} />}
+      {selectedTab === 1 && selectedMenu === "SeekBoiler" && <SeekWaterRecord type="Boiler" />}
       {selectedTab === 1 && selectedMenu === "RegisterBoiler" && <RegisterWaterRecord type="Boiler" />}
 
       {selectedTab === 1 && selectedMenu === "SeekBlowmold" && <SeekWaterRecord type="Blowmold" />}
--- water-test-ui/water-tests/src/Components/Dashboard/WaterTreatmentSubmenu/SeekWaterRecord/index.js
+++ water-test-ui/water-tests/src/Components/Dashboard/WaterTreatmentSubmenu/SeekWaterRecord/index.js
@@ -1,16 +1,59 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import useToken from "../../../../Middleware/useToken"
 
+import axios from "axios"
+import { Spin } from "antd"
 export const SeekWaterRecord = (props) => {
-  if (props.data) {
+
+  const { token } = useToken()
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  const boilerwater = [
+    "boilerwater",
+    "feedwater",
+    "condensatewater",
+    "rawwater",
+    "softwater"
+  ]
+  const coolingtower = [
+    "blowmoldglycolwater",
+    "coolingtowerwater",
+    "chilledglycolwater"
+  ]
+
+  switch(props.type){
+    case "Boiler": 
+                  var requests = boilerwater.map((url)=>{
+                    return axios.get("http://localhost:8080/api/"+url,{params: {token}})
+                  })
+    break
+  }
+  useEffect(()=>{
+    Promise.allSettled(requests).then((responses) => {
+      setData(responses)
+      
+    }).finally(setLoading(false))
+  })
+
+
+  if (loading) {
+    return <Spin delay="50" style={{ margin: "auto" }} />
+  }
+  if (data) {
     return (
       <h1>
-        ID: {props.data._id} <br />
-        Created: {props.data.createdAt} <br />
-        PH: {props.data.ph} <br />
-        Alkalinity: {props.data.alkalinity} <br />
-        Conductivity: {props.data.conductivity} <br />
+        ID: {boilerwater._id} <br />
+        Created: {boilerwater.createdAt} <br />
+        PH: {boilerwater.ph} <br />
+        Alkalinity: {boilerwater.alkalinity} <br />
+        Conductivity: {boilerwater.conductivity} <br />
       </h1>
     )
+  }
+  else {
+    <h1>error</h1>
   }
 }
 
